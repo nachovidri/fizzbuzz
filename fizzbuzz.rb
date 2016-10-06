b9f2dab22ff0ac46a4d0288d0addcb03701a5d54
Nacho Vidri <nacho@pompeiibrand.com>
2016-10-06 21:46:46 +0200

    second commit

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,1 +1,44 @@
-puts "hola feo"
+
+#USING FOR LOOP
+
+# for i in 1..100
+# 	if i%3 == 0 && i%5 == 0
+# 		puts "FizzBuzz"
+# 	elsif i%3 == 0
+# 		puts "Fizz"
+# 	elsif i%5 ==0
+# 		puts "Buzz"
+# 	else
+# 		puts i
+# 	end		
+# end
+
+
+#USING WHILE LOOP
+
+i = 0
+while i < 100
+	
+	if i%3 == 0 && i%5 == 0
+		puts "FizzBuzz"
+	elsif i%3 == 0
+		puts "Fizz"
+	elsif i%5 ==0
+		puts "Buzz"
+	else
+		puts i
+	end
+i = i + 1
+
+end
+
+
+
+
+
+
+
+
+
+
+
