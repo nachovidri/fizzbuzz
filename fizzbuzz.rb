f74aa134ad4c8042aaf9a5b57f973234c1fc4351
Nacho Vidri <nacho@pompeiibrand.com>
2016-10-11 10:44:32 +0200

    iteration 4 done, exercise completed

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -19,15 +19,30 @@
 i = 0
 while i < 100
 	
-	if i%3 == 0 && i%5 == 0
-		puts "FizzBuzz"
-	elsif i%3 == 0
-		puts "Fizz"
-	elsif i%5 ==0
-		puts "Buzz"
-	else
-		puts i
+	result = ""
+	aux = 0
+	
+	if i%3 == 0
+		result << "Fizz"
+		aux = 1
 	end
+
+	if i%5 == 0
+		result << "Buzz"
+		aux = 1
+	end
+
+	if i == 1 || i/10 == 1
+		result << "Bang"
+		aux = 1
+	end
+
+	if aux == 0
+		result = i
+	end
+
+puts result
+
 i = i + 1
 
 end
